856445f5907b05fe835fcd2a1b3ff1d36ef6062d
masafefe <marticanmet@gmail.com>
2021-06-14 22:13:24 +0200

    adding classifiers and catalan data

---
+++ classifier/.ipynb_checkpoints/sentiment_textblob-checkpoint.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 27 23:09:38 2021
+
+@author: marti
+"""
+#%% Libraries and data
+import pandas as pd
+from textblob import TextBlob
+
+df = pd.read_csv('data/EN/vacunes_100rt_en.csv', index_col=0)
+df = df[df.columns[1:]]
+
+#%% Sentiment
+def get_polarity(text):
+    return TextBlob(text).sentiment
+
+sentiment = df['content'].apply(get_polarity)
+
+df['polarity'] = sentiment.apply(lambda x: x[0])
+df['subjectivity'] = sentiment.apply(lambda x: x[1])
+
+#%%
+df.to_csv('data/EN/vacunes_100rt_en_textblob.csv')
